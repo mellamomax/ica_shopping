ce33477aa3bf05a48141ef3cd2c51fa3015ec122
Mellamomax <max.skold@hotmail.com>
2025-06-01 20:09:26 +0200

    fdds

--- custom_components/ica_shopping/sensor.py
+++ custom_components/ica_shopping/sensor.py
@@ -1,3 +1,8 @@
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
 from datetime import timedelta
 from homeassistant.components.sensor import SensorEntity
 
@@ -9,6 +14,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     """Set up ICA Shopping List sensors."""
     api = hass.data[DOMAIN][DATA_ICA]
     lists = await api.async_fetch_lists()  # ✅ Använd async-funktionen
+    _LOGGER.debug("Fetched %d shopping lists: %s", len(lists), lists)
     entities = [ShoppingListSensor(api, lst) for lst in lists]
     async_add_entities(entities, True)
 
@@ -36,10 +42,12 @@ class ShoppingListSensor(SensorEntity):
     async def async_update(self):
         """Update sensor data."""
         lists = await self._api.async_fetch_lists()
+        _LOGGER.debug("Updating list %s", self._list.get("id"))
         for lst in lists:
             if lst.get("id") == self._list.get("id"):
                 self._list = lst
                 self._attr_extra_state_attributes["items"] = [
                     item["text"] for item in lst.get("items", [])
                 ]
+                _LOGGER.debug("Updated list %s with %d items", lst.get("name"), len(lst.get("items", [])))
                 break
